d018f0b696b25b644ab3d63eafa1dbb123653fa9
Joseph Gordy <jgordy2424@gmail.com>
2017-08-12 11:04:27 -0400

    Completed fetch request and display of the required elements + styling

--- starter_files/main.js
+++ starter_files/main.js
@@ -1,28 +1,29 @@
-/*
-  Here is a rough idea for the steps you could take:
-*/
 
 // 1. First select and store the elements you'll be working with
-let results = document.getElementsByClassName("results");
-
-let  music_player = document.getElementsByClassName("music-player");
-
-
-
-
-
-
 
 // 2. Create your `submit` event for getting the user's search term
 
-
-
-
-
-
 // 3. Create your `fetch` request that is called after a submission
 
-fetch("https://itunes.apple.com/search?term=1989&limit=12")
+// 4. Create a way to append the fetch results to your page
+
+// 5. Create a way to listen for a click that will play the song in the audio play
+
+let results = document.getElementById("results");
+
+let  music_player = document.getElementsByClassName("music-player");
+
+// add event listener to form
+// let submit = document.querySelector(".search-form");
+// submit.addEventListener("submit", function(event) {
+//   results.innerHTML = "";
+//   let search = document.getElementById("search");
+//
+// let info = {
+//   url: "https://itunes.apple.com/search?term=jack+johnson&limit=12"
+// }
+
+fetch("https://itunes.apple.com/search?term=jack+johnson&limit=15")
   .then(
     function(response) {
       if (response.status !== 200) {
@@ -30,24 +31,35 @@ fetch("https://itunes.apple.com/search?term=1989&limit=12")
         return;
       }
       response.json().then(function(data) {
-        console.log("Here is the data:", data);
+        console.log("Here is the data:", data.results);
+
+        for (var i = 0; i < data.results.length; i++) {
+          let songContainer = document.createElement("div");
+          songContainer.setAttribute("class", "songContainer")
+
+          let albumImage = document.createElement("img")
+          albumImage.setAttribute("src", `${data.results[i].artworkUrl100}`)
+          let songTitle = document.createElement("p");
+          let bandName = document.createElement("h5");
+
+          results.appendChild(songContainer);
+          songContainer.appendChild(albumImage);
+          songContainer.appendChild(songTitle);
+          songContainer.appendChild(bandName);
+
+          songTitle.textContent = `${data.results[i].trackName}`;
+
+          bandName.textContent = `${data.results[i].artistName}`;
+
+          songContainer.addEventListener("click", function(event) {
+
+          })
+        }
       });
     }
   )
   .catch(function(err) {
     console.log("Fetch Error :-S", err);
   });
-
-
-
-
-
-
-// 4. Create a way to append the fetch results to your page
-
-
-
-
-
-
-// 5. Create a way to listen for a click that will play the song in the audio play
+// }
+// );
