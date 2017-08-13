c9258f9ced211ba15a312c8283a4acf45521184f
Joseph Gordy <jgordy2424@gmail.com>
2017-08-13 13:59:35 -0400

    imported some images for this project styling

--- starter_files/main.js
+++ starter_files/main.js
@@ -1,10 +1,7 @@
 
 // 2. Create your `submit` event for getting the user's search term
 
-// 5. Create a way to listen for a click that will play the song in the audio player
-
 let results = document.getElementById("results");
-
 let submit = document.getElementById("submitButton");
 
 submit.addEventListener("click", function() {
@@ -12,10 +9,7 @@ submit.addEventListener("click", function() {
   results.innerHTML = "";
 
   let searchInput = document.getElementById("searchInput");
-   console.log(searchInput.value);
   let str = searchInput.value.split(" ").join("+");
-   console.log(str);
-
   let searchEntry = "https://itunes.apple.com/search?term=" + str + "&entity=song&limit=15";
 
   fetch(searchEntry)
@@ -26,7 +20,6 @@ submit.addEventListener("click", function() {
           return;
         }
         response.json().then(function(data) {
-          console.log("Here is the data:", data);
 
           for (var i = 0; i < data.results.length; i++) {
             let songContainer =   document.createElement("div");
@@ -46,23 +39,19 @@ submit.addEventListener("click", function() {
 
             bandName.textContent = `${data.results[i].artistName}`;
 
-            //  let index = i;
-// console.log(data.results[i].previewUrl);
-albumImage.value = i;
-// console.log(albumImage.value);
+            albumImage.value = i;
+
             albumImage.addEventListener("click", function(event) {
-              console.log(event);
-              console.log(event.target);
-console.log(event.target.value);
+
               playSong(event.target.value);
             })
             function playSong(index) {
               let musicPlayer = document.getElementById("music-player");
-              console.log(musicPlayer);
+
               let selectedSong =  data.results[index].previewUrl;
-              console.log(selectedSong);
+
               musicPlayer.setAttribute("src", selectedSong);
-              console.log(musicPlayer.src);
+
               musicPlayer.load();
               musicPlayer.play();
             }
